23df8880d554546222d1b7210c9e752b11b6bb15
Haspp <107359050+Haspp@users.noreply.github.com>
2023-04-17 23:48:00 +0300

    Delete homework1_task2.java

--- homework1_task2.java
+++
@@ -1,9 +0,0 @@
-public class homework1_task2 {
-    public static void main(String[] args) {
-        
-        for (int i = 1; i <= 1000; i+=2){
-            System.out.print(" " + i);
-        }
-        
-    }
-}
